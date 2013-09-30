52d8d21549e69b49227c4547498abb414a580084
GBH <gbh@gbh-MS-7788.(none)>
2013-09-30 09:53:55 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'dd8dab253bf087dbcfa284b702856a236a17e84844ca3a55a1346c4d6154da840b5ae0212dace7a5c2cbe8ccab2331e5fe9a38b705a153995c616da9a8063aae'
