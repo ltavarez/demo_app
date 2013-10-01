de337223b27416174a7b31a9e1f6c1b923afbe96
GBH <gbh@gbh-MS-7788.(none)>
2013-10-01 16:38:11 -0400

    el email se ponde minuscula antes de ser insertado o actualizado para mantener la validacion

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,4 +1,5 @@
 class Usuario < ActiveRecord::Base
+  before_save {self.email = email.downcase}
   validates :nombre, presence: true , length: {maximum: 50}
   validates :email,  presence: true, uniqueness: {case_sensitive: false}
 end
